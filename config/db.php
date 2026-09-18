293b65e659d6fb0fad6afc255c6a651910db0f3b
portrcr <portrcr@gmail.com>
2026-09-18 15:13:38 +0300

    created db connection

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+	$host		= "localhost";
+	$dbname		= "employee_db";
+	$user		= "root";
+	$pass		= "";
+	$charset	= "utf8mb4";
+
+	$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+	$options = [
+		PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,	// Throw exceptions on errors
+		PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC,		// Return data as associative arrays
+		PDO::ATTR_EMULATE_PREPARES		=> false,					// Use real prepared statements
+	];
+
+	try {
+		$pdo = new PDO($dsn, $user, $pass, $options);
+
+		echo "Connection successful";
+	} catch (\PDOException $e) {
+		error_log($e->getMessage());
+		die("Database connection failed");
+	}
+	
